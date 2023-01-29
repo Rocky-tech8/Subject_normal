b3f0834bf9c9f974507a95e3182787e6e04989d9
Rocky-tech8 <k.rocky.riu@gmail.com>
2023-01-29 07:08:39 +0000

    フォーム構成を作成

---
+++ form-app/components/Form.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <p>お問い合わせフォーム</p>
+    <form>
+      <dl>
+        <dt>氏名【必須】</dt>
+        <dd>
+            <input type="text" name="name" size="7" maxlength="51" required>
+        </dd>
+        <dt>性別【必須】</dt>
+        <dd>
+          <input type="radio" name="gender" value="0" required checked>男性
+          <input type="radio" name="gender" value="1" required>女性
+          <input type="radio" name="gender" value="2" required>その他
+        </dd>
+        <dt>生年月日【必須】</dt>
+        <dd>
+            <input type="date" name="birthday" required>
+        </dd>
+        <dt>〒郵便番号【必須】</dt>
+        <dd>
+          <input type="text" name="postnumber" required>
+        </dd>
+        <dt>住所【必須】</dt>
+        <dd>
+          <input type="text" name="address" maxlength="201" required>
+        </dd>
+        <dt>電話番号【任意】</dt>
+        <dd>
+          <input type="tel" name="phone">
+        </dd>
+        <dt>メールアドレス【任意】</dt>
+        <dd>
+          <input type="text" name="email" maxlength="201">
+        </dd>
+        <dt>お問い合わせの種類【必須】</dt>
+        <dd>
+          <select name="question1" required>
+            <option value="0">新規お取引のご相談</option>
+            <option value="1">会社資料請求</option>
+            <option value="2">セミナーのご相談</option>
+            <option value="3">プログラムのご相談</option>
+            <option value="4">広報・取材依頼などのお問い合わせ</option>
+            <option value="5">その他</option>
+        </select>
+        </dd>
+        <dt>相談内容【必須】</dt>
+        <dd>
+          <textarea name="question2" rows="10" cols="50" maxlength="1001" required></textarea>
+        </dd>
+      </dl>
+      <input type="submit" value="送信">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
